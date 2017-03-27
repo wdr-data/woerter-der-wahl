236a2aa9a1681c1ff511082c9ab0e00e00d983d1
mraerino <melodie124@gmail.com>
2017-03-27 18:30:50 +0200

    :sparkles: Open graph on bubble click

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import BubbleCloud from './lib/bubble_cloud';
     const exampleData = [];
     for(let i = 0; i < 40; i++) {
         exampleData.push({
+            id: i,
             word: "test",
             count: Math.floor(Math.exp(Math.random()*10)*40)
         });
