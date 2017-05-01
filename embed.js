34c47b0a3834bf6c7ca75e5ac2ecf6bea5e619a3
mraerino <melodie124@gmail.com>
2017-05-01 17:06:37 +0200

    :iphone: Optimize embed for mobile

--- embed.js
+++ embed.js
@@ -1,17 +1,37 @@
 import BubbleCloud from './lib/bubble_cloud';
 import qs from 'qs';
 import * as helpers from './lib/data_helpers';
+import debounce from 'lodash/debounce';
 
 (function() {
     const params = qs.parse(window.location.search.substr(1));
+
+    const wordCountGetter = () => window.innerWidth > 425 ? 30 : 20;
+    let wordCount = wordCountGetter();
+    let cloud = null;
+    let data = [];
 
     fetch('output/top30.json')
         .then(res => res.json())
         .then(json => {
             const bubbles = json.data;
-            const data = helpers.bubbleData(bubbles, params.party || 'all').map(helpers.prepareData).slice(0, 30);
+            data = helpers.bubbleData(bubbles, params.party || 'all')
+                .map(helpers.prepareData);
+            const currentData = data.slice(0, wordCount);
             const elem = document.getElementById('bubbleCloudVis');
             elem.classList.add('party-'+(params.party || 'all'));
-            BubbleCloud(elem).setData(data);
+            cloud = BubbleCloud(elem);
+            cloud.setData(currentData);
         });
+
+    window.addEventListener('resize', debounce(() => {
+        if(!cloud) {
+            return;
+        }
+        const former = wordCount;
+        wordCount = wordCountGetter();
+        if(former !== wordCount) {
+            cloud.setData(data.slice(0, wordCount));
+        }
+    }, 500))
 })();
