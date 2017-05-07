763dec6d27bfa8bc4c2ec9c80dc8ec5791c072cb
mraerino <melodie124@gmail.com>
2017-05-07 16:35:52 +0200

    :construction_worker: Include embed lib in development server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,6 +2,10 @@ const webpack = require('webpack');
 const prod = require('./webpack.config');
 
 module.exports = Object.assign({}, prod, {
+    entry: {
+        'lib':   './lib',
+        'embed': './embed.js'
+    },
     devtool: 'cheap-source-map',
     plugins: []
 });
