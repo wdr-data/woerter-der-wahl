80f89507335df915b0ea0ecb4d12a6fb6fe1872b
mraerino <melodie124@gmail.com>
2017-05-07 17:56:43 +0200

    :art: Allow custom words split by plain +

--- embed.js
+++ embed.js
@@ -14,7 +14,7 @@ import 'whatwg-fetch';
     const params = qs.parse(window.location.search.substr(1));
 
     const party = params.party || 'all';
-    const customWords = (params.customwords || '').split('+');
+    const customWords = (params.customwords || '').replace(/\+/g, ' ').split(' ');
 
     const wordCountGetter = () => customWords.length === 0 ? (window.innerWidth > 425 ? 30 : 20) : customWords.length;
 
